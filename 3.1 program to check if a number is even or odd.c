dc2e7e2f4e9d96c7d1635ce1bb72a02332ab184c
Arun Sharma <146552177+arun1645@users.noreply.github.com>
2023-10-16 22:18:23 +0530

    Add files via upload

---
+++ 3.1 program to check if a number is even or odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+     printf("%d is an even value",a);
+
+    }
+    else
+    {
+      printf("%d is an odd value",a);
+    }
+    return 0;
+}
